6a325e576612367c7b421edd9a0263d0926de9b0
resolutpartners <104563047+resolutpartners@users.noreply.github.com>
2026-01-20 11:57:53 +0530

    Day 18: Full CI/CD Pipeline

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello from GitHub Actions! This image was built automatically.');
+app.get("/", (req, res) => {
+  res.send("Hello from Day 18! I was deployed automatically by a robot.");
 });
 
 app.listen(port, () => {
